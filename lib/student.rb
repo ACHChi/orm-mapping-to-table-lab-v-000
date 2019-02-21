97f1cfe588bc90789d80d84b00e30a8bb0064f54
Chao Hsuan Chi <allen0413@gmail.com>
2019-02-21 04:35:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id: nil, name:, grade:)
+  def initialize(id = nil, name, grade)
     @id, @name, @grade = id, name, grade
   end
+
+  def self.create_table
 
   def self.save
 
